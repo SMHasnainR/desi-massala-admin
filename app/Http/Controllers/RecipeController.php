a24a8a1953410f35be245c9b2915872d7618e8ee
hasnain <hasnainshoaib45@gmail.com>
2022-10-28 21:44:36 +0500

    dfsd

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Recipe;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
@@ -11,8 +12,23 @@ class RecipeController extends Controller
 
     public function index(){
         $recipes =  Recipe::where('status', 1)->get()->take(9);
+        $title = 'Vegetable Recipes';
+
+
+
         $categories = Category::all();
-        return view('recipes.index', compact('recipes','categories'));
+        return view('recipes.index', compact('recipes','categories','title'));
+    }
+
+    public function vegRecipes(){
+
+        $recipes =  Recipe::whereHas('category', function(Builder $query){
+            $query->where('name','Vegetable');
+        })->where('status', 1)->get()->take(9);
+        $title = 'Vegetable Recipes';
+
+        $categories = Category::all();
+        return view('recipes.index', compact('recipes','categories','title'));
     }
 
     public function show(Recipe $recipe){
