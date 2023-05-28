658149a76bcb280abfee7018655aff98a8c44495
Hasnain <hasnain.shoaib@naheed.pk>
2023-05-28 13:40:29 +0500

    Home Slider Fixed

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
     <!-- bradcam_area  -->
     <div class="bradcam_area bradcam_bg_1">
         <div class="container">
@@ -24,9 +23,12 @@
                     <div class="recepie_text text-center">
                         <h3>Recipe videos that never <br>
                             misses any portion</h3>
-<div class="embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" width="870" height="415" src="https://www.youtube.com/embed/13KhGiKPV8w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" width="870" height="415"
+                                src="https://www.youtube.com/embed/13KhGiKPV8w" title="YouTube video player" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen></iframe>
+                        </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                             laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
@@ -41,5 +43,4 @@
         </div>
     </div>
     <!--/ recepie_header_area  -->
-
 @endsection
